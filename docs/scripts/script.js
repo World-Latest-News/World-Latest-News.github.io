5272472ecd175de318a94722c2e35f0f5c0050dc
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-09-20 04:37:31 +0500

    updates

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -18,3 +18,12 @@ if ("serviceWorker" in navigator) {
         );
 }
 window.addEventListener("load", run);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-45CH0H0F54");
+
