95101edf1c8270dccc826a4c5727f747e31e9829
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-09-21 02:28:22 +0500

    updated

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("service-worker.js", {
+        .register("scripts/service-worker.js", {
             scope: ".",
         })
         .then(
